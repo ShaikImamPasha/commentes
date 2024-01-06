89e1947666f21f1c77efd4bfd4e029331ab580d6
sk imam pashga <122773374+ShaikImamPasha@users.noreply.github.com>
2024-01-06 02:08:48 -0800

    Add files via upload

--- CommentApp.js
+++ CommentApp.js
@@ -1,44 +1,38 @@
 import { useEffect, useState } from "react";
-import io, { Socket } from "socket.io-client";
+import io from "socket.io-client";
 import { ComponentFun } from "./ComponentFun";
 import dotenv from 'dotenv';
 dotenv.config()
 import MiniCmpt from "./MiniCmpt";
+
 const CommentApp = () => {
   const [commentsData, setCommentsData] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const socket = io(process.env.Back_End_Socket_Api_Url);
 
   useEffect(() => {
+    socket.emit('requestInitialData', { restaurantId: 32 });
 
-    socket.emit('requestInitialData', { restaurantId: 212 });
-
-    // Listen for the initial data response from the server
     socket.on('initialData', (initialData) => {
-      console.log(initialData);
       setCommentsData(initialData);
     });
-    // Listen for new comments
+
     socket.on('newComment', (newComment) => {
-      console.log("new",newComment);
-       setCommentsData(newComment)
+      setCommentsData(newComment);
     });
 
     socket.on('newReply', (initialData) => {
-      console.log(initialData);
       setCommentsData(initialData);
     });
+
     return () => {
-      // Disconnect the socket when the component unmounts
       socket.disconnect();
     };
   }, []);
 
-
   const pushComment = () => {
-    // Emit a socket event to add a new comment
     socket.emit('addComment', {
-      restaurantId: 212,
+      restaurantId: 32,
       newComment: {
         name: "imam",
         message: searchValue,
@@ -49,16 +43,23 @@ const CommentApp = () => {
   };
 
   return (
-    <div>
-      <div>
+    <div className="container mx-auto p-4 md:p-8">
+      <div className="mb-4">
         <input
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="add comment"
+          placeholder="Add a comment"
           type="text"
-        ></input>
-        <button onClick={pushComment}>add comment</button>
+          className="border p-2 w-full md:w-1/2"
+        />
+        <button
+          onClick={pushComment}
+          className="bg-blue-500 text-white px-4 py-2 mt-2 md:mt-0 md:ml-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none"
+        >
+          Add Comment
+        </button>
       </div>
+
       {commentsData && commentsData.length === 0 ? (
         <p className="w-[100px] h-[100px] border-2 border-solid"></p>
       ) : (
